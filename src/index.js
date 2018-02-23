87c0428630ee2ef4ab417baceca3e4fbfcbe93d7
Ganozri <Ganozri@users.noreply.github.com>
2018-02-23 11:47:53 +0300

    "solution"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-module.exports = function getZerosCount(number, base) {
-  // your implementation
+module.exports = function getZerosCount(number, base)
+{
+var S=0;
+  var max;
+var del;
+for(var i=2;i<=base;i++)
+{
+	if(base%i==0)
+		{
+        del=0;
+        for(var k=2;k<=i;k++)
+        	{
+        if(i%k==0){del++;}
+        	}
+        if(del==1)
+        	{
+        max=i;
+        	}
+		}
+}
+
+     while (number >= max)
+     {
+       number = Math.floor(number/max);
+       S = S + number;
+     }
+ return (S);
 }
